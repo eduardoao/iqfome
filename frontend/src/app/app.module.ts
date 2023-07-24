b28ed8c51d3b2cab6b715591816eb604f0b9960d
EDUARDO ALCANTARA DE OLIVEIRA <eduardo.alcantara@trtrio.gov.br>
2023-07-24 11:39:34 -0300

    Page not found created and inserted it in another pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 
 
@@ -25,7 +26,8 @@ import { TitleComponent } from './components/partials/title/title.component';
     TagsComponent,
     FooterComponent,
     CartPageComponent,
-    TitleComponent
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
