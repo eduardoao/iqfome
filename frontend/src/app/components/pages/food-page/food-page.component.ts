1c9568e6e453cc43d95d8ad6145dda279632b13a
EDUARDO ALCANTARA DE OLIVEIRA <eduardo.alcantara@trtrio.gov.br>
2023-07-25 09:25:42 -0300

    up time API

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -16,7 +16,10 @@ export class FoodPageComponent {
     private cartService:CartService, private route:Router) {
     activatedRoute.params.subscribe((params) => {
       if(params.id)
-        this.food = foodService.getFoodById(params.id);
+        //this.food = foodService.getFoodById(params.id);
+        foodService.getFoodById(params.id).subscribe(serverFood => {
+          this.food = serverFood;
+        });
     });
   }
 
