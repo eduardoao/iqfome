465f3ba2016aaac3cc555f03df414f6ba8c7fbb6
Eduardo <eoalcantara@gmail.com>
2023-08-18 21:22:50 -0300

    Connection with moongoDB finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
-
-import { sample_foods, sample_tags, sample_users } from './data';
+import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
+import { dbConnect } from './configs/database.config';
+dbConnect();
 
 const app = express();
 
@@ -13,65 +17,10 @@ app.use(cors({
     origin:['http://localhost:4200', 'https://iqfome.onrender.com']
 }));
 
-app.get('/api/foods', (req, res) => {   
-    console.log(sample_foods) ;
-    res.send(sample_foods);
-});
 
-app.get('/api/foods/search/:searchTerm', (req, res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods = sample_foods
-    .filter(food => food.name.toLowerCase()
-    .includes(searchTerm.toLowerCase()));
+app.use('/api/foods', foodRouter);
+app.use('/api/users', userRouter);
 
-    console.log(foods) ;
-
-    res.send(foods);    
-});
-
-app.get('/api/foods/tags', (req, res) =>{
-
-    res.send(sample_tags);
-});
-
-app.get('/api/foods/tag/:tagName', (req, res) =>{
-    const tagName = req.params.tagName;
-    const foods = sample_foods.filter(food => food.tags?.includes(tagName));
-
-    res.send(foods);
-});
-
-app.get('/api/foods/:foodId', (req, res) =>{
-    const foodId = req.params.foodId;
-    const food = sample_foods.find(food => food.id == foodId);
-    res.send(food);
-});
-
-app.post("/api/users/login", (req, res) => {
-    const {email, password} = req.body;
-    let user = sample_users.find(user => user.email === email 
-      && user.password === password);
-      
-  
-     if(user) {
-      res.send(generateTokenReponse(user));
-     }
-     else{
-       const BAD_REQUEST = 400;
-       res.status(BAD_REQUEST).send("Usuário ou senha inválido(s)!");
-     }
-  
-  })
-
-  function generateTokenReponse(user: any): any {
-    const token = jwt.sign(
-        {email:user.email, isAdmin:user.isAdmin}, 
-        "SomeRandonText", 
-        {expiresIn:"10d"});    
-    
-        user.token = token;
-    return user;    
-  }
 
 
 const PORT = 5000;
